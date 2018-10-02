fca2857795d051397c51cb1472f616242fc0d126
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2018-10-02 12:51:32 +0200

    add favicon :)

--- run-jekyll.sh
+++ run-jekyll.sh
@@ -4,9 +4,11 @@ echo -e "Gonne!\n\n"
 
 echo -e "If you want to create output with drafts run:"
 echo -e "sh $0 --drafts\n\n"
+echo -e "If you want verbose output run:"
+echo -e "sh $0 --verbose\n\n"
 
 echo -e "Running a jekyll server $1 at 127.0.0.1:4000\n\n"
-bundle exec jekyll serve $1
+bundle exec jekyll serve --lsi $1
 
 echo -e "\n\n"
 echo "Oh, you are evil! you killed jekyll server! :O"
