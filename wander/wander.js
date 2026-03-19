dbc8a7335314d6ed38aa84cdc8c01389654aa114
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2026-03-19 11:06:24 -0300

    Initialize a wander console
    See https://news.ycombinator.com/item?id=47422759

---
+++ wander/wander.js
@@ -0,0 +1,13 @@
+window.wander = {
+  // Other Wander consoles that visitors can reach from my console.
+  consoles: [
+    'https://susam.net/wander/',
+  ],
+  // My favourite websites and pages I recommend to the Wander community.
+  pages: [
+    'https://susam.net/',
+    'https://sunshowers.io/',
+    'https://julianaklulo.dev/en/',
+    'https://jvns.ca/'
+  ],
+}
