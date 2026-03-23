85b6fef39138088ba0178d10f36c65e2345c3709
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2026-03-23 11:04:37 -0300

    wander: expand list of consoles and pages
    Signed-off-by: Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>

--- wander/wander.js
+++ wander/wander.js
@@ -2,12 +2,15 @@ window.wander = {
   // Other Wander consoles that visitors can reach from my console.
   consoles: [
     'https://susam.net/wander/',
+    'https://tuftel.today/wander/',
+    'https://martincapodici.com/wander/'
   ],
   // My favourite websites and pages I recommend to the Wander community.
   pages: [
     'https://susam.net/',
     'https://sunshowers.io/',
     'https://julianaklulo.dev/en/',
-    'https://jvns.ca/'
+    'https://jvns.ca/',
+    'https://discord.com/blog/why-discord-is-switching-from-go-to-rust'
   ],
 }
